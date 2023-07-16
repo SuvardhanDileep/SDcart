87a3e6594c05eabf49e3d7cbd91bbd6661223deb
SuvardhanDileep <gsuvardhandileep@gmail.com>
2023-07-16 12:33:55 +0530

    feat: sdcart

---
+++ src/components/Announcement/Announcement.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Announcement.css' 
+function Announcement() {
+  return (
+    <div className="announcement">
+        Suvardhan Dileep SD-Cart
+    </div>
+  )
+}
+
+export default Announcement
